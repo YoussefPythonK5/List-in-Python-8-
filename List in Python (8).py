b1cb86cc556ccf286d8fd3152b1748614979194e
Youssef <ya103857@gmail.com>
2026-05-12 20:57:35 +0300

    Add files via upload

---
+++ List in Python (8).py
@@ -0,0 +1,13 @@
+# List
+# Tools :
+# remove()
+fruits = ["apple","orange","kiwi"]
+print ("Before",fruits)
+fruits.remove("orange")
+print ("After",fruits)
+print ("*"*40)
+# del
+fruits = ["apple","orange","kiwi"]
+print ("Before",fruits)
+del fruits[0]
+print ("After",fruits)
